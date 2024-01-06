aac7310d5dd81262818a940346280608d3c8923d
vit73123 <vit73123@gmail.com>
2024-01-06 16:15:09 +0300

    feat: 05 Iterators, Generators: Sem

--- tasks/main/L05_Iterators_Generators/sem/t07+.py
+++ tasks/main/L05_Iterators_Generators/sem/t07+.py
@@ -2,17 +2,16 @@
 # ✔ Функция генерирует N простых чисел, начиная с числа 2.
 # ✔ Для проверки числа на простоту используйте правило: «число является простым, если делится
 # нацело только на единицу и на себя».
-
-
 N = 100
 START = 2
 
 
 def is_prime(num: int) -> bool:
-    for i in range(START, num):
+    for i in range(2, num):
         if num / i == num // i:
             return False
     return True
+
 
 # Генератор
 def prime_num(count: int):
